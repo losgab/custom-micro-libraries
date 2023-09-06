8d029894583a46cc1cf8e469cdd6568196394726
losgab <53280648+losgab@users.noreply.github.com>
2023-09-06 13:00:42 +1000

    docstrings added for button funcitons

--- esp32/Button/Button.h
+++ esp32/Button/Button.h
@@ -19,26 +19,37 @@ typedef struct Button
 
 typedef Button* button_t;
 
-/*
-    Initialises a button attached to the specified pin
-    (1 for true) is_input_pullup
+/**
+ * @brief Initialises a button attached to the specified pin
+ * 
+ * @param pin GPIO pin that button is attached to
+ * @param is_input_pullup 1 -> INPUT_PULLUP, 0 for external
+ * 
+ * @return Pointer to struct malloc
 */
 button_t create_button(uint8_t pin, uint8_t is_input_pullup);
 
-/*
-    Reads and updates the state of the button
+/**
+ * @brief Reads and updates the state of the button
+ * 
+ * @param button struct Button *
 */
 void update_button(button_t button);
 
-/*
-    Returns pressed state of button
-    1 -> Pressed
-    0 -> Not Pressed
+/**
+ * @brief Returns whether button was pushed
+ * 
+ * @param button struct Button *
+ * 
+ * @return 1 if pushed, 0 if not. Works on pulses
 */
 uint8_t was_pushed(button_t button);
 
-/*
-    Returns 1 if button is held down, else 0
-    Dependent on debounce time
+/**
+ * @brief Returns whether button is being held down
+ * 
+ * @param button struct Button *
+ * 
+ * @return 1 if held down, 0 if not.
 */
 uint8_t is_pressed(button_t button);
