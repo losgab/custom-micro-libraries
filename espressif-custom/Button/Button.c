ed84a110c7e6ae5bddc1754e61bdfcb3395790a6
Gabriel Thien <53280648+losgab@users.noreply.github.com>
2024-01-07 02:43:34 +1100

    fixed bug with conditional pullup in configuration

--- espressif-custom/Button/Button.c
+++ espressif-custom/Button/Button.c
@@ -4,8 +4,9 @@ button_t create_button(uint8_t pin, uint8_t is_input_pullup)
 {
     button_t new_button = malloc(sizeof(Button));
 
-    gpio_set_direction(pin, GPIO_MODE_INPUT);    
-    gpio_pullup_en(pin);
+    gpio_set_direction(pin, GPIO_MODE_INPUT);
+    if (is_input_pullup)
+        gpio_pullup_en(pin);
 
     new_button->pin = pin;
     new_button->last_state = 0;
@@ -16,8 +17,8 @@ button_t create_button(uint8_t pin, uint8_t is_input_pullup)
 
 void update_button(button_t button)
 {
-	uint8_t reading = !gpio_get_level(button->pin);
-	button->pushed = (!button->last_state && reading);
+    uint8_t reading = !gpio_get_level(button->pin);
+    button->pushed = (!button->last_state && reading);
     button->last_state = reading;
 }
 
