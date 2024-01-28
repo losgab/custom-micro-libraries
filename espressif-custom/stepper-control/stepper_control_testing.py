41dbbeb16df5dea04043a47801acade1af1b05f3
Gabriel Thien <53280648+losgab@users.noreply.github.com>
2024-01-28 23:20:41 +1100

    stepper angle estimation achieved

--- espressif-custom/stepper-control/stepper_control_testing.py
+++ espressif-custom/stepper-control/stepper_control_testing.py
@@ -12,17 +12,23 @@ DEGREE_1 = 91
 DEGREE_2 = 92
 
 def round_to_stepper_res(angle: float) -> float:
-    rounded = floor(angle / STEP_RESOLUTION) * STEP_RESOLUTION
+    rounded = round(angle / STEP_RESOLUTION) * STEP_RESOLUTION
     return rounded
 
 def steps_req(angle):
     return angle / 1.8
 
+def degree_error(angle, stepper_angle):
+    return abs(angle - stepper_angle)
+
 print("---------------------------")
 for i in range(0, 91, 1):
-    result = round_to_stepper_res(i)
-    stepper_driver_conversion["[" + str(i) + "] Computed Stepper Degree"] = result
-    stepper_driver_conversion["[" + str(i) + "] Steps Required"] = steps_req(result)
+    stepper_angle = round_to_stepper_res(i)
+    steps_required = steps_req(stepper_angle)
+    error = degree_error(i, stepper_angle)
+    stepper_driver_conversion["[" + str(i) + "] Computed Stepper Degree"] = round(stepper_angle, 1)
+    stepper_driver_conversion["[" + str(i) + "] Steps Required"] = round(steps_required, 1)
+    stepper_driver_conversion["[" + str(i) + "] Error"] = round(error, 2)
     # print(f"[{i}] Steps Required: {steps_req(result)}")
 print("---------------------------")
 
